0ce58672c96ea3180369bef41df494e5380bf508
Arka Poddar <poddararka27@gmail.com>
2018-08-03 06:41:27 -0400

    Add files via upload

---
+++ src/app/workprofile/workprofile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workprofile',
+  templateUrl: './workprofile.component.html',
+  styleUrls: ['./workprofile.component.css']
+})
+export class WorkprofileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
