d3c1f305c1cb5706ec56049a2bdc6cac3ccaef5c
Arka Poddar <poddararka27@gmail.com>
2018-08-03 06:38:53 -0400

    Add files via upload

---
+++ src/app/officelocation/officelocations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-officelocations',
+  templateUrl: './officelocations.component.html',
+  styleUrls: ['./officelocations.component.css']
+})
+export class OfficelocationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
